14bf5a2384f75eb463df43b4deab8e635c379855
Frederic BIDON <fredbi@yahoo.com>
2023-11-11 10:32:09 +0100

    fixed lock
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- lock.go
+++ lock.go
@@ -12,24 +12,35 @@ const lockTableSuffix = "_lock"
 
 func (m Migrator) ensureLockTable(ctx context.Context) error {
 	tableName := fmt.Sprintf("%s%s", goose.TableName(), lockTableSuffix)
+	db := m.DB
 
-	if _, err := m.DB.ExecContext(ctx, fmt.Sprintf(
+	if _, err := db.ExecContext(ctx, fmt.Sprintf(
 		`CREATE TABLE IF NOT EXISTS %s (status INTEGER NOT NULL DEFAULT 0)`, tableName,
 	)); err != nil {
 		return err
 	}
 
-	if _, err := m.DB.ExecContext(ctx, fmt.Sprintf(
+	cancellable, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	tx, err := db.BeginTx(cancellable, nil)
+	if err != nil {
+		return err
+	}
+
+	if _, err = tx.ExecContext(cancellable, fmt.Sprintf(
 		`DELETE FROM %s`, tableName,
 	)); err != nil {
 		return err
 	}
 
-	_, err := m.DB.ExecContext(ctx, fmt.Sprintf(
+	if _, err = tx.ExecContext(ctx, fmt.Sprintf(
 		`INSERT INTO %s(status) VALUES(0)`, tableName,
-	))
+	)); err != nil {
+		return err
+	}
 
-	return err
+	return tx.Commit()
 }
 
 func (m Migrator) acquireLock(ctx context.Context) (*sql.Tx, error) {
@@ -43,7 +54,7 @@ func (m Migrator) acquireLock(ctx context.Context) (*sql.Tx, error) {
 
 	row := tx.QueryRowContext(ctx,
 		fmt.Sprintf(
-			`SELECT MAX(status) FROM %s`, tableName,
+			`SELECT MAX(status) FROM %s FOR UPDATE`, tableName,
 		),
 	)
 
